f6d103c68f59964db9d87df212ffc6a3b3751448
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-06-11 12:58:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,11 @@ st.balloons()
 st.markdown("# Data Evaluation App")
 
 st.write("hi this is rakesh")
+def read_excel(file):
+    return pd.read_excel(file)
 
+# Streamlit app
+st.title("Excel File Viewer")
+
+# File uploader widget
+uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls", "csv"])
