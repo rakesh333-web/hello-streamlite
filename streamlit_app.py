a899423bb6d188adc3529e03a78595887a29f0f5
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-06-11 13:29:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,4 @@ st.title("Excel File Viewer")
 # File uploader widget
 uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls", "csv"])
 st.write("generate the excel sheet")
-df=pd.read_file(uploaded_file)
-print(df)
+
