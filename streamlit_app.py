bc8e047398b5ff6e2218ba581d04ae710fc6fa32
rakesh333-web <85956046+rakesh333-web@users.noreply.github.com>
2024-06-11 16:24:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,15 @@ import pandas as pd
 st.markdown("# Data Evaluation App")
 
 st.write("HI: this is B.rakesh welcome ")
-def read_excel(file):
-    return pd.read_excel(file)
+
 
 
 st.title("Excel File Viewer")
 
 # File uploader widget
-uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls", "csv"])
+
 st.write("generate the excel sheet")
-if uploaded_file is not None:
+
     df=pd.read_excel(EXAMPLEINPUT.xlsx)
     st.dataframe(df)
     st.write(df.to_string())
